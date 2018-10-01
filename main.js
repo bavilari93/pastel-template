04590f8fa19dbb8fa910034c2205bd7e80e8b490
Bianca A <frank@Bianca-Avila-4.local>
2018-10-01 18:08:43 +0900

    final

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log('works ');
+
+window.onload = (()=>{
+	const container = document.querySelector('.nav');
+	const aTag = container.querySelectorAll('a');
+	console.log(aTag);
+	aTag.forEach((e)=>{
+
+		e.addEventListener('click', ((event)=>{
+			e.className===''?
+			e.className += 'select' :
+			e.classList.remove('select');
+	
+		}))
+	
+	})
+})
+// when using jquery 
+// $('body').on('click', 'li', function() {
+//       $('li.active').removeClass('active');
+//       $(this).addClass('active');
+// });
